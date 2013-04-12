63f3b7ab5203a1a51f5c22a218a7c9788aa471aa
Irina <irrillka@gmail.com>
2013-04-12 11:25:57 +0400

    Initial project

---
+++ womensdays/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  womensdays
+//
+//  Created by Ирина Завилкина on 12.04.13.
+//  Copyright (c) 2013 zavilkina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WDAppDelegate class]));
+    }
+}
